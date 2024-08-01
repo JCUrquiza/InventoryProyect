a746ce0063b4f31aeebb88af9258a853f7831ef3
Juan Carlos <gurquiza8@gmail.com>
2024-08-01 14:44:01 -0600

    feature: add new dto(create customer dto)

--- src/domain/dtos/customers/create-customer.dto.ts
+++ src/domain/dtos/customers/create-customer.dto.ts
@@ -8,11 +8,42 @@ export class CreateCustomerDto {
         public readonly email: string,
         public readonly address: string,
         public readonly phone: string,
+        public readonly typeCustomerId: number,
+        public readonly branchOfficeId: number,
     ) {}
 
     static create( props: {[key: string]: any} ): [string?, CreateCustomerDto?] {
+        const { name, apellidoPaterno, apellidoMaterno, email, address, phone, typeCustomerId, branchOfficeId } = props;
 
-        return [];
+        if ( !name ) return ['The name needs to be added', undefined];
+        if ( !apellidoPaterno ) return ['The apellidoPaterno needs to be added', undefined];
+        if ( !apellidoMaterno ) return ['The apellidoMaterno needs to be added', undefined];
+        if ( !email ) return ['The email needs to be added', undefined];
+        if ( !address ) return ['The address needs to be added', undefined];
+        if ( !phone ) return ['The phone needs to be added', undefined];
+        if ( !typeCustomerId ) return ['The typeCustomerId needs to be added', undefined];
+        if ( !branchOfficeId ) return ['The branchOfficeId needs to be added', undefined];
+
+        // Capitalizar y recortar:
+        const nameTrimmed = name.trim();
+        const nameCapitalized = nameTrimmed.charAt(0).toUpperCase() + nameTrimmed.slice(1).toLowerCase();
+        const apellidoPaternoTrimmed = apellidoPaterno.trim();
+        const apellidoPaternoCapitalized = apellidoPaternoTrimmed.charAt(0).toUpperCase() + apellidoPaternoTrimmed.slice(1).toLowerCase();
+        const apellidoMaternoTrimmed = apellidoMaterno.trim();
+        const apellidoMaternoCapitalized = apellidoMaternoTrimmed.charAt(0).toUpperCase() + apellidoMaternoTrimmed.slice(1).toLowerCase();
+
+        return [
+            undefined,
+            new CreateCustomerDto(
+                nameCapitalized,
+                apellidoPaternoCapitalized,
+                apellidoMaternoCapitalized,
+                email,
+                address,
+                phone,
+                typeCustomerId,
+                branchOfficeId)
+        ];
     }
 
 }
