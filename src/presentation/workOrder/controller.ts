41d920242fdb6fcc1a85bd42eb7f1240dc1e8334
Juan Carlos <gurquiza8@gmail.com>
2024-08-08 14:20:39 -0600

    feature: improve the response of creating workOrder

--- src/presentation/workOrder/controller.ts
+++ src/presentation/workOrder/controller.ts
@@ -65,15 +65,81 @@ export class WorkOrderController {
                     id: newWorkOrder.id,
                 },
                 include: {
+                    customers: {
+                        include: {
+                            typeCustomer: true,
+                            branchOffice: true,
+                            status: true
+                        }
+                    },
+                    status: true,
                     productsInWorkOrder: {
                         include: {
-                            products: true
+                            products: {
+                                include: {
+                                    productFamily: true
+                                }
+                            }
                         }
                     }
                 }
             });
 
-            return res.status(201).json({ workOrder: completeWorkOrder });
+            const productsOfWorkOrder = completeWorkOrder?.productsInWorkOrder.map( ( product) => ({
+                idproductWorkOrder: product.id,
+                quantity: product.quantity,
+                workOrderId: product.workOrderId,
+                product: {
+                    id: product.products.id,
+                    name: product.products.name,
+                    codigoSKU: product.products.codigoSKU,
+                    salePrice: product.products.salePrice,
+                    productFamily: {
+                        id: product.products.productFamily.id,
+                        name: product.products.productFamily.name,
+                    }
+                }
+            }));
+        
+            const response = {
+                id: completeWorkOrder?.id,
+                address: completeWorkOrder?.address,
+                priceOfLabor: completeWorkOrder?.priceOfLabor,
+                priceOfTransfer: completeWorkOrder?.priceOfTransfer,
+                priceTotal: completeWorkOrder?.priceTotal,
+                customer: {
+                    id: completeWorkOrder?.customers.id,
+                    name: completeWorkOrder?.customers.name,
+                    apellidoPaterno: completeWorkOrder?.customers.apellidoPaterno,
+                    apellidoMaterno: completeWorkOrder?.customers.apellidoMaterno ,
+                    email: completeWorkOrder?.customers.email,
+                    address: completeWorkOrder?.customers.address,
+                    telephone: completeWorkOrder?.customers.telephone,
+                    typeCustomer: {
+                        id: completeWorkOrder?.customers.typeCustomer.id,
+                        name: completeWorkOrder?.customers.typeCustomer.name
+                    },
+                    branchOffice: {
+                        id: completeWorkOrder?.customers.branchOffice.id,
+                        name: completeWorkOrder?.customers.branchOffice.name
+                    },
+                    status: {
+                        id: completeWorkOrder?.customers.status.id,
+                        name: completeWorkOrder?.customers.status.name,
+                        code: completeWorkOrder?.customers.status.code,
+                        color: completeWorkOrder?.customers.status.color
+                    }
+                },
+                status: {
+                    id: completeWorkOrder?.status.id,
+                    name: completeWorkOrder?.status.name,
+                    code: completeWorkOrder?.status.code,
+                    color: completeWorkOrder?.status.color
+                },
+                products: productsOfWorkOrder
+            }
+
+            return res.status(201).json({ workOrder: response });
 
         } catch (error) {
             console.log(error);
